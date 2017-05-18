b05cb88e829dd2a1585d8b2a83cd632617d47274
xubojoy <xubojoy1112@163.com>
2017-05-18 18:04:58 +0800

    自定义urls

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,12 +1,23 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 # Register your models here.
+
+#有关联的可以值注册一个
+#StackedInline
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,      {'fields':['question_text']}),
         ('日期信息', {'fields':['pub_date'],'classes':['collapse']}),
     ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text','pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 
 admin.site.register(Question,QuestionAdmin)
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request,question_id):
+    return HttpResponse('you are looking at question %s.' % question_id)
+
+def results(request, question_id):
+    response = 'you are looking at the results of question %s.'
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse('you are voting on question %s.' % question_id)
